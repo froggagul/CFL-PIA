b7438a57cb7a43524eca7cdf8d4ef59c73704cc2
froggagul <froggagul@gmail.com>
2022-12-06 17:46:53 +0900

    feat: new experiment with race - glasses_sunglasses

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -7,7 +7,7 @@ for index in ${!data_seed[*]}; do
     for var in 4
     do
         warmup=100
-	pycmd="main.py -t gender -a race --pi 2 -ni 150 -c 0 -nc 3 -nw $var -ds ${data_seed[$index]} -ms ${model_seed[$index]} --project attack9"
+	pycmd="main.py -t race -a glasses --pi 1 -ni 150 -c 0 -nc 3 -nw $var -ds ${data_seed[$index]} -ms ${model_seed[$index]} --project test"
 	echo $pycmd
 	python $pycmd
     done
