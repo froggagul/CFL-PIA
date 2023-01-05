e8e3214098ee296f2cf4965cbf75aef8490f2e51
blingcho <q1w1er1t@gmail.com>
2023-01-05 16:59:56 +0900

    Revert "save path"
    This reverts commit 62ca5e18f59328e06592bb5553736c7495a43999.

--- scripts/dp_smile_glass/dp_run_0.sh
+++ scripts/dp_smile_glass/dp_run_0.sh
@@ -8,7 +8,7 @@ for index in ${!data_seed[*]}; do
     for var in 5 10 15 20 25
     do
         warmup=100
-	pycmd="main.py -t smile -a glasses --pi 1 -ni 5000 -c ${gpu} -nc 3 -nw $var -ds ${data_seed[$index]} -ms ${model_seed[$index]} --project attack_dp -dp -ep ${eps}"
+	pycmd="main.py -t smile -a glasses --pi 1 -ni 5000 -c ${gpu} -nc 3 -nw $var -ds ${data_seed[$index]} -ms ${model_seed[$index]} --project attack9 -dp -ep ${eps}"
 	echo $pycmd
 	python $pycmd
     done
